80440fc3b6dd5d886eb854bd672f4627e64ed169
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-06-11 13:28:13 -0300

    Segundo teste de lógica.

---
+++ script.js
@@ -0,0 +1,15 @@
+let idade=19
+if (idade >=18){
+  console.log('Acesso Liberado!')
+}
+
+let quantosAnos=22
+console.log (quantosAnos >=18, 'Ok. Você está liberado!')
+
+let mesAniversário='Janeiro'
+if(mesAniversário == 'Janeiro' || mesAniversário == 'Dezembro'){
+  console.log('Oba! Em breve assopraremos velinhas!')
+}
+else{
+  console.log('Vamos esperar um pouco mais para cantar parabéns pra você!')
+}
