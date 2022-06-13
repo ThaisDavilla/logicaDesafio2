6051c9451a18f7124fd0288fb6ddcc3aa1b99313
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-06-13 12:11:37 -0300

    Correção da questão 2. Inclusão de estilo. Adição de p, h2 e imagem.

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let idade=19
-if (idade >=18){
+let idade = 19
+if (idade >= 18) {
   console.log('Acesso Liberado!')
 }
 
-let quantosAnos=22
-console.log (quantosAnos >=18, 'Ok. Você está liberado!')
-
-let mesAniversário='Janeiro'
-if(mesAniversário == 'Janeiro' || mesAniversário == 'Dezembro'){
-  console.log('Oba! Em breve assopraremos velinhas!')
+let quantosAnos = 22
+let naoRobo = true
+if (quantosAnos >= 18 && naoRobo == true){
+  console.log('Ok. Pode acessar!')
 }
 else{
+  console.log('Acesso negado.')
+}
+
+
+
+  let mesAniversário = 'Janeiro'
+if (mesAniversário == 'Janeiro' || mesAniversário == 'Dezembro') {
+  console.log('Oba! Em breve assopraremos velinhas!')
+}
+else {
   console.log('Vamos esperar um pouco mais para cantar parabéns pra você!')
 }
+
+
